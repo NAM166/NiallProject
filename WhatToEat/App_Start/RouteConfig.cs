5d44e8ff6b2f1e2426b43686c314414a0d6d2aa4
Niall McClelland <niallmcclelland166@hotmail.com>
2019-06-03 18:58:23 +0100

    Add project files.

---
+++ WhatToEat/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WhatToEat
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "WhatToEat.Controllers" });
+
+            routes.MapRoute("Diary", "Diary/{action}/{id}", new { controller = "Diary", action = "Index", id = UrlParameter.Optional }, new[] { "WhatToEat.Controllers" });
+
+            routes.MapRoute("Eatery", "Eatery/{action}/{name}", new { controller = "Eatery", action = "Index", name = UrlParameter.Optional }, new[] { "WhatToEat.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "WhatToEat.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "WhatToEat.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "WhatToEat.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "WhatToEat.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
---
+++ WhatToEat/Models/ViewModels/Diary/DiaryVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WhatToEat.Models.ViewModels.Diary
+{
+    public class DiaryVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Calorie { get; set; }
+        public int Total { get { return Quantity * Calorie; } }
+        public string Image { get; set; }
+    }
+}
